b86c9dce727751b5974fb8df8f8294ea2c22e9a9
Erik <erikdmitchell@gmail.com>
2018-08-22 09:50:07 -0400

    removed updater since it’s filled with errors

--- pickle-admin-notices.php
+++ pickle-admin-notices.php
@@ -56,7 +56,6 @@ final class PickleAdminNotices {
 	}
 
 	public function includes() {
-    	include_once(PAN_PATH.'updater/updater.php');
 		include_once(PAN_PATH.'admin/class-pickle-admin-notices-admin.php');
 		
 		if (is_admin()) :
@@ -72,33 +71,6 @@ final class PickleAdminNotices {
 
 	public function init() {
 
-	}
-	
-	public function plugin_updater() {
-		if (!is_admin())
-			return false;
-	
-		if (!defined('PAN_GITHUB_FORCE_UPDATE'))
-			define('PAN_GITHUB_FORCE_UPDATE', true);
-			
-		$username='erikdmitchell';
-		$repo_name='pickle-admin-notices';
-		$folder_name='pickle-admin-notices';
-	    
-	    $config = array(
-	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-	        'proper_folder_name' => $folder_name, // this is the name of the folder your plugin lives in
-	        'api_url' => 'https://api.github.com/repos/'.$username.'/'.$repo_name, // the github API url of your github repo
-	        'raw_url' => 'https://raw.github.com/'.$username.'/'.$repo_name.'/master', // the github raw url of your github repo
-	        'github_url' => 'https://github.com/'.$username.'/'.$repo_name, // the github url of your github repo
-	        'zip_url' => 'https://github.com/'.$username.'/'.$repo_name.'/zipball/master', // the zip url of the github repo
-	        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-	        'requires' => '4.0', // which version of WordPress does your plugin require?
-	        'tested' => '4.9', // which version of WordPress is your plugin tested up to?
-	        'readme' => 'readme.txt', // which file to use as the readme for the version number
-	    );
-	   
-		new PAN_GitHub_Updater($config);
 	}	
 
 }
